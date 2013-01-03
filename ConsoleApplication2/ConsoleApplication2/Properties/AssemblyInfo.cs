ab9d64aa5d2e33c87db5f3d456508cc2d6ca4ea0
apropoapo <apropoapo@hotmail.de>
2013-01-03 02:16:36 +0100

    0.0.10 Test COnsole TCP receive (Server)

--- ConsoleApplication2/ConsoleApplication2/Properties/AssemblyInfo.cs
+++ ConsoleApplication2/ConsoleApplication2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1_test")]
+[assembly: AssemblyTitle("ConsoleApplication2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1_test")]
+[assembly: AssemblyProduct("ConsoleApplication2")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0ff6836e-4bc3-427c-98f4-2660fb22d260")]
+[assembly: Guid("fbfd94cd-23e2-46d7-830e-36fa466e1da4")]
 
 // Version information for an assembly consists of the following four values:
 //
