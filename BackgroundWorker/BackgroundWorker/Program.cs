763240401e06b3c63e59bd604c6eb35ce0bb7b80
apropoapo <apropoapo@hotmail.de>
2013-01-05 01:58:57 +0100

    0.1.1 Alles Neu

---
+++ BackgroundWorker/BackgroundWorker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace BackgroundWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long i = 0;
+            Users myUsers = new Users();
+            bool exit = false;
+
+            myUsers.updateUserArray(true);
+
+            while (!exit)
+            {
+                Console.WriteLine("---------------------------- Neuer Durchlauf --------------------------------");
+
+                myUsers.updateUserArray(false);
+
+                string[] sArray = myUsers.checkUsers();
+                foreach (string s in sArray)
+                {
+                    if (s != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+
+
+                Thread.Sleep(10000);
+
+            }
+        }
+    }
+}
--- BackgroundWorker/BackgroundWorker/Properties/AssemblyInfo.cs
+++ BackgroundWorker/BackgroundWorker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication2")]
+[assembly: AssemblyTitle("BackgroundWorker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication2")]
+[assembly: AssemblyProduct("BackgroundWorker")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fbfd94cd-23e2-46d7-830e-36fa466e1da4")]
+[assembly: Guid("e223e8b6-00ff-42c4-a45c-ffa87f1699c9")]
 
 // Version information for an assembly consists of the following four values:
 //
